1ff88af8887ef44364cdecab079754e445adb3bd
jiwyatt <jiwyatt@cisco.com>
2021-11-16 15:39:44 -0600

    1. moved application.properties outside of jar file 2. upgraded some Angular packages
    TODO:
    add SSL config to application.properties
    Update documentation

---
+++ src/test/java/com/cisco/app/dbconnector/DbConnectorTests.java
@@ -0,0 +1,13 @@
+//package com.cisco.app.dbconnector;
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//class DbConnectorTests {
+//
+//	@Test
+//	void contextLoads() {
+//	}
+//
+//}
